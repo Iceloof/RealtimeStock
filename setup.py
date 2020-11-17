68a3219bb875760ba91613da1cb33e454cfa1965
hurinhu <hurin@live.ca>
2020-11-17 15:12:09 +1300

    update release version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="RealtimeStock",
-    version="1.0.1",
+    version="1.0.2",
     author="Hurin Hu",
     author_email="hurin@live.ca",
     description="Realtime stock quote for Python",
